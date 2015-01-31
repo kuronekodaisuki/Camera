cb0be419a561dc7465cf73eaffa7d66f0ee6b1cb
kuronekodaisuki <kuronekodaisuki@me.com>
2015-01-31 21:00:07 +0900

    Create Camera.h

---
+++ Camera.h
@@ -0,0 +1,37 @@
+//
+// Camera.h for Camera enumeration
+//
+
+#include <opencv2/highgui/highgui.hpp>
+
+#ifdef _MSC_VER
+#include <windows.h>
+#include <dshow.h>
+#endif
+
+using namespace cv;
+
+typedef bool (*PENUMDEVICE)(void *pItem, char *pDeviceName);
+
+class Camera: VideoCapture
+{
+public:
+	static int EnumDevices(PENUMDEVICE pCallback, void *pItem);
+
+	/*
+		Usage:
+		
+		// enum callback function 
+		static int enumCamera(void* pItem, char *pName)
+		{
+			printf("%s\n", pName);
+			return 0;
+		}
+
+		int main()
+		{
+			int cameraCount;
+			cameraCount = Camera::EnumDevices((PENUMDEVICE)&enumCamera, NULL);
+		}
+	*/
+};
